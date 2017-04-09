a7040ffa9ce76289acc10a6e1419857aa2e2efe1
xiekun1992 <840914927@qq.com>
2017-04-09 21:22:09 +0800

    添加自定义标题标识的功能

--- src/catelogue.js
+++ src/catelogue.js
@@ -16,10 +16,20 @@
 		this.catelogue = [];
 		this.elementOffsetTop = [];
 		this.titles = {};
+		this.titleTags = ["H1", "H2", "H3", "H4", "H5", "H6"];
+		this.useCustomTitles = false;
 
 		this.selector = options.contentContainer;
 		this.showBackToTop = options.showBackToTop;
-		(typeof this.showBackToTop === 'undefined') && (this.showBackToTop = true);
+		if(Object.prototype.toString.call(options.customTitles) === '[object Array]' && options.customTitles.length > 0 && options.customTitleAttr){
+			this.titleTags = options.customTitles;
+			this.customTitleAttr = options.customTitleAttr;
+			this.useCustomTitles = true;
+		}
+
+		if(typeof this.showBackToTop === 'undefined'){
+			this.showBackToTop = true;
+		}
 		if(this.selector){
 			this.element = document.querySelector(this.selector);
 			if(!this.element){
@@ -39,12 +49,14 @@
 	};
 	xCatelogue.prototype.extractTitle = function(){
 		// 考虑搜索引擎对h的规则，h1应当只有一个，所以从h2开始计算标题
-		this.titles.h1 = Array.from(document.querySelectorAll(this.selector + ' h1'));
-		this.titles.h2 = Array.from(document.querySelectorAll(this.selector + ' h2'));
-		this.titles.h3 = Array.from(document.querySelectorAll(this.selector + ' h3'));
-		this.titles.h4 = Array.from(document.querySelectorAll(this.selector + ' h4'));
-		this.titles.h5 = Array.from(document.querySelectorAll(this.selector + ' h5'));
-		this.titles.h6 = Array.from(document.querySelectorAll(this.selector + ' h6'));
+		for(var i = 0; i < this.titleTags.length; i++){
+			var t = this.titleTags[i].toLowerCase();
+			var tFlag = t;
+			if(this.useCustomTitles){
+				tFlag = '*[' + this.customTitleAttr + '="' + t + '"]';
+			}
+			this.titles[t] = Array.prototype.slice.call(document.querySelectorAll(this.selector + ' ' + tFlag));
+		}
 		setTitlesId(this.titles);
 	};
 	xCatelogue.prototype.generateCatelogue = function(forceRefresh){
@@ -56,19 +68,22 @@
 	};
 	xCatelogue.prototype.updateCatelogue = function(){
 		root = {self: this.element, children: this.catelogue, parent: null};
-		for(var c of Array.from(this.element.children)){
-			if(["H1", "H2", "H3", "H4", "H5", "H6"].indexOf(c.tagName) != -1){
+		var children = Array.prototype.slice.call(this.element.children);
+		for(var i = 0; i < children.length; i++){
+			var c = children[i];
+			if(this.titleTags.indexOf(this.getTitleFlag(c)) != -1){
 				// label offsetTop
 				this.elementOffsetTop.push({offsetTop: countOffsetTop(c), id: c.getAttribute('id')});
 			}
 		}
 		this.elementOffsetTop.push({offsetTop: Infinity});
-		seekForHierarchy(this.element.children[0], root);
+		this.seekForHierarchy(this.element.children[0], root);
 	};
 	xCatelogue.prototype.bootstrap = function(){
-		var as = Array.from(document.querySelectorAll('.x-catelogue a'));
+		var as = Array.prototype.slice.call(document.querySelectorAll('.x-catelogue a'));
 		clicked = false;
-		for(var a of as){
+		for(var i = 0; i < as.length; i++){
+			var a = as[i];
 			if(a.getAttribute('href').indexOf('#') == 0){
 				a.addEventListener('click', function(e){
 					// console.log('click')
@@ -77,24 +92,67 @@
 				}, false);
 			}
 		}
-		onscroll.call(this);
-		window.addEventListener('scroll', onscroll.bind(this));
+		if(this.catelogue.length){
+			onscroll.call(this);
+			window.addEventListener('scroll', onscroll.bind(this));
+		}
 
 	};
-	function highlight(hash){
+	xCatelogue.prototype.seekForHierarchy = function(element, tree){
+		if(element instanceof HTMLElement){
+			var node = {self: element, children: [], parent: tree};
+
+			var tagName = this.getTitleFlag(element);
+			var tagNameIndex = this.titleTags.indexOf(tagName);
+			var nextElement = element.nextElementSibling;
+
+			if(tagNameIndex != -1){
+				tree.children.push(node);
+			}
+			if(nextElement){
+				var pos = this.titleTags.indexOf(this.getTitleFlag(nextElement));
+				if(pos == -1){ //非标题标签
+					this.seekForHierarchy(nextElement, (tagNameIndex != -1?node: tree));
+				}else if(tagNameIndex != -1 && pos > tagNameIndex){ //子标题
+					this.seekForHierarchy(nextElement, node);
+				}else if(pos == tagNameIndex){ //同级标题
+					this.seekForHierarchy(nextElement, tree);
+				}else{ //父级标题
+					var parentNode = tree;
+					//非标题元素寻找下一元素的父级标题
+					while(parentNode && this.titleTags.indexOf(this.getTitleFlag(parentNode.self)) >= this.titleTags.indexOf(this.getTitleFlag(nextElement))){
+						parentNode = parentNode.parent;
+					}
+					if(tagNameIndex != -1){
+						this.seekForHierarchy(nextElement, parentNode && parentNode.parent || root);
+					}else{
+						this.seekForHierarchy(nextElement, parentNode && parentNode || root);
+					}
+				}
+			}
+		}
+	};
+	xCatelogue.prototype.getTitleFlag = function(element){
+		if(this.useCustomTitles){
+			return element.getAttribute(this.customTitleAttr);
+		}
+		return element.tagName;
+	};
+
+	var highlight = function(hash){
 		// console.log(hash)
 		if(hash.length > 1){
-			var as = Array.from(document.querySelectorAll('.x-catelogue a'));
-			for(var a of as){
-				a.classList.remove('active');
+			var as = Array.prototype.slice.call(document.querySelectorAll('.x-catelogue a'));
+			for(var i = 0; i < as.length; i++){
+				as[i].classList.remove('active');
 			}
 			var currentHash = document.querySelectorAll("a[href='" + hash + "']");
 			currentHash && currentHash.forEach(function(h){
 				h.classList.add('active');
 			});
 		}
-	}
-	function onscroll(){
+	};
+	 var onscroll = function(){
 		// scroll会在click触发完成之后执行，使用哨兵clicked去除多余的highlight调用
 		if(clicked) return clicked = false;
 		// 滚动到底了
@@ -103,7 +161,8 @@
 			id = this.elementOffsetTop[this.elementOffsetTop.length - 2].id;
 		}else{
 			var pre = this.elementOffsetTop[0];
-			for(var e of this.elementOffsetTop){
+			for(var i = 0; i < this.elementOffsetTop.length; i++){
+				var e = this.elementOffsetTop[i];
 				// 计算出现的偏移量容错
 				if(e.offsetTop == window.scrollY || e.offsetTop == window.scrollY + 1){
 					// console.log(e);
@@ -118,7 +177,7 @@
 			}
 		}
 		highlight('#' + id);
-	}
+	};
 
 	var root, clicked = false;
 
@@ -149,43 +208,7 @@
 		var offsetTop = element.offsetTop, offsetParent = element.offsetParent;
 		return offsetParent && (offsetTop + countOffsetTop(offsetParent)) || offsetTop;
 	};
-	var seekForHierarchy = function(element, tree){
-		if(element instanceof HTMLElement){
-			var node = {self: element, children: [], parent: tree};
-
-			var titleTags = ["H1", "H2", "H3", "H4", "H5", "H6"];
-			var tagName = element.tagName;
-			var tagNameIndex = titleTags.indexOf(tagName);
-			var nextElement = element.nextElementSibling;
-
-			if(tagNameIndex != -1){
-				tree.children.push(node);
-			}
-			if(nextElement){
-				var pos = titleTags.indexOf(nextElement.tagName);
-				if(pos == -1){ //非标题标签
-					seekForHierarchy(nextElement, (tagNameIndex != -1?node: tree));
-				}else if(tagNameIndex != -1 && pos > tagNameIndex){ //子标题
-					// node.children.push(nextElement);
-					seekForHierarchy(nextElement, node);
-				}else if(pos == tagNameIndex){ //同级标题
-					// peers.push(nextElement);
-					seekForHierarchy(nextElement, tree);
-				}else{ //父级标题
-					var parentNode = tree;
-					//非标题元素寻找下一元素的父级标题
-					while(parentNode && titleTags.indexOf(parentNode.self.tagName) >= titleTags.indexOf(nextElement.tagName)){
-						parentNode = parentNode.parent;
-					}
-					if(tagNameIndex != -1){
-						seekForHierarchy(nextElement, parentNode && parentNode.parent || root);
-					}else{
-						seekForHierarchy(nextElement, parentNode && parentNode || root);
-					}
-				}
-			}
-		}
-	};
+	
 
 	return xCatelogue;
 });
